1b408bec07eb45cb4a7bcff605b3dd37e8c25f38
Carol Greene <carolgreene3@gmail.com>
2018-11-17 13:29:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function loadIssue() {
   var issue = {
     state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:14 UTC",
-    body: "Instructions say GET /team and POST/newteam. Rspec wants GET/newteam and POST/team."
-    
+    number: 5, 
+    created_at: "2016-03-31 16:23:13 UTC",
+    body: "Instructions say GET /team and POST/newteam. RSPEC wants GET/newteam and POST/team."
   }
 
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
