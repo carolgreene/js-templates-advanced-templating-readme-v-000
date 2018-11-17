1ad3c07c26f47cf9bf0bc8f0df17514faef4424a
Carol Greene <carolgreene3@gmail.com>
2018-11-17 13:18:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-Handlebars.registerHelper('comment_body', function() {
-  if(this.state === 'closed') {
-    return new Handlebars.SafeString(this.body)
-  } else {
-    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+function loadIssue() {
+  var issue = {
+    state: "closed",
+    number: 5,
+    created_at: "2016-03-31 16:23:14 UTC",
+    body: "Instructions say GET /team and POST/newteam. Rspec wants GET/newteam and POST/team."
+    
   }
-})
 
-
-
-
-function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  var result = template(issues);
+  var result = template(issue);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
